cc3d4882d32f4cf0538d40218353767c28b58fa8
niravgohil <nirav2018@gmail.com>
2019-03-12 15:50:40 +0530

    initial commit

--- src/AllPost.js
+++ src/AllPost.js
@@ -7,7 +7,7 @@ class AllPost extends Component {
   render() {
     return (
     <div>
-      <h1>All asasdasdasdasPosts</h1>
+      <h1>All Posts</h1>
            {this.props.posts.map((post) => (
                     <div key={post.id}>
                         {post.editing ? <EditComponent post={post} key={post.id} /> :
